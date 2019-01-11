f2ad1362ae20b981ee9b349fca31ee4720f5e6d4
Benson Imoh, ST <benson@stbensonimoh.com>
2019-01-11 13:09:05 +0100

    Add Styles to Stylesheet and Validation * add styles to css file * add form validation class to index.html * add validation script to main.js script file

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+( () => {
+    'use strict';
+    window.addEventListener('load', () => {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        let forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        let validation = Array.prototype.filter.call(forms, (form) => {
+            form.addEventListener('submit', (event) => {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+
+
+    }, false);
+})();

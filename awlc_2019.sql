5d6c4abc634f15bf83ef4a5cb05ab242b497140e
Anita Jarikre <anitajarikre55@gmail.com>
2019-01-14 15:30:04 +0100

    created the database for the form and new branch

---
+++ awlc_2019.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 14, 2019 at 02:36 PM
+-- Server version: 5.7.24-0ubuntu0.18.04.1
+-- PHP Version: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `AWLCRwanda2019`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AWLC_2019`
+--
+
+CREATE TABLE `AWLC_2019` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `firstName` text NOT NULL,
+  `lastName` text NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `country` text NOT NULL,
+  `organisation` text NOT NULL,
+  `member` enum('yes','no') NOT NULL,
+  `referrer` text NOT NULL,
+  `firstConference` enum('yes','no') NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `AWLC_2019`
+--
+ALTER TABLE `AWLC_2019`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `AWLC_2019`
+--
+ALTER TABLE `AWLC_2019`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
